2144e5fa8a4ae2b9b6d3f8cc2c34203f355b9222
juliajykim <jkim24@gwu.edu>
2021-05-24 08:51:11 -0400

    heroku test

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -7,30 +6,32 @@ const passport = require('passport');
 const Tweet = require('../../models/Tweet');
 const validateTweetInput = require('../../validation/tweets');
 
-router.get("/test", (req, res) => res.json({ msg: "This is the tweet route" }));
-
 router.get('/', (req, res) => {
-  Tweet
-    .find()
+  Tweet.find()
     .sort({ date: -1 })
     .then(tweets => res.json(tweets))
-    .catch(err => res.status(404).json(err));
+    .catch(err => res.status(404).json({ notweetsfound: 'No tweets found' }));
 });
 
 router.get('/user/:user_id', (req, res) => {
   Tweet.find({ user: req.params.user_id })
     .sort({ date: -1 })
     .then(tweets => res.json(tweets))
-    .catch(err => res.status(404).json(err));
+    .catch(err =>
+      res.status(404).json({ notweetsfound: 'No tweets found from that user' }
+      )
+    );
 });
 
 router.get('/:id', (req, res) => {
   Tweet.findById(req.params.id)
     .then(tweet => res.json(tweet))
-    .catch(err => res.status(404).json(err));
+    .catch(err =>
+      res.status(404).json({ notweetfound: 'No tweet found with that ID' })
+    );
 });
 
-router.post("/",
+router.post('/',
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
     const { errors, isValid } = validateTweetInput(req.body);
@@ -40,8 +41,8 @@ router.post("/",
     }
 
     const newTweet = new Tweet({
-      user: req.user.id,
-      text: req.body.text
+      text: req.body.text,
+      user: req.user.id
     });
 
     newTweet.save().then(tweet => res.json(tweet));
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
-const keys = require('../../config/keys_dev'));
+const keys = require('../../config/keys');
 const passport = require('passport');
 
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 
+router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
   res.json({
@@ -24,6 +25,7 @@ router.post('/register', (req, res) => {
   if (!isValid) {
     return res.status(400).json(errors);
   }
+
 
   User.findOne({ email: req.body.email })
     .then(user => {
@@ -49,8 +51,11 @@ router.post('/register', (req, res) => {
     })
 })
 
+
 router.post('/login', (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);
+
+  console.log(errors);
 
   if (!isValid) {
     return res.status(400).json(errors);
@@ -68,7 +73,8 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            const payload = { id: user.id, handle: user.handle, email: user.email };
+            const payload = { id: user.id, name: user.name };
+
             jwt.sign(
               payload,
               keys.secretOrKey,
@@ -79,7 +85,6 @@ router.post('/login', (req, res) => {
                   token: 'Bearer ' + token
                 });
               });
-
           } else {
             return res.status(400).json({ password: 'Incorrect password' });
           }
