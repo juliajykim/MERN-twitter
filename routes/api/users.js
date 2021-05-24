3ad9bb022071c9452dc6b4ce61ef6b3d13b22e81
juliajykim <jkim24@gwu.edu>
2021-05-24 08:34:50 -0400

    heroku test

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
-// const keys = require('../../config/keys');
+const keys = require('../../config/keys');
 const passport = require('passport');
 
 const validateRegisterInput = require('../../validation/register');
@@ -86,50 +86,5 @@ router.post('/login', (req, res) => {
         })
     })
 })
-
-router.post("/register", (req, res) => {
-  // const { errors, isValid } = validateRegisterInput(req.body);
-
-  // if (!isValid) {
-  //   return res.status(400).json(errors);
-  // }
-
-  User.findOne({ handle: req.body.handle }).then(user => {
-    if (user) {
-      errors.handle = "User already exists";
-      return res.status(400).json(errors);
-    } else {
-      const newUser = new User({
-        handle: req.body.handle,
-        email: req.body.email,
-        password: req.body.password
-      });
-
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser
-            .save()
-            .then(user => {
-              const payload = { id: user.id, handle: user.handle };
-
-              jwt.sign(
-                payload,
-                keys.secretOrKey,
-                { expiresIn: 3600 },
-                (err, token) => {
-                  res.json({
-                    success: true,
-                    token: "Bearer " + token
-                  });
-                });
-            })
-            .catch(err => console.log(err));
-        });
-      });
-    }
-  });
-});
 
 module.exports = router;
