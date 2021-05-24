f04fe9cb255e3fb400e9d7e3763da38210e0c7e4
juliajykim <jkim24@gwu.edu>
2021-05-24 01:39:39 -0400

    heroku test

--- app.js
+++ app.js
@@ -2,24 +2,24 @@ const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const passport = require('passport');
+
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-const User = require("./models/User")
-const bodyParser = require('body-parser');
 
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("========Connected to MongoDB successfully========="))
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(bodyParser.json());
-app.get("/", (req, res) => res.send("Hello World"));
+
+app.get("/", (req, res) => res.send("Hello World!!"));
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
