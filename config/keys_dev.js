117f88ea87e04bcdde6deb98b99d34ed601f4018
juliajykim <jkim24@gwu.edu>
2021-05-24 08:53:20 -0400

    gix gitignore

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://admin:7BAZyzy7mpg98Z9g@mern.i7vfa.mongodb.net/MERNdb?retryWrites=true&w=majority',
-  secretOrKey: 'qlalfghrdmsdufthl'
-  //Make sure this is your own unique string
-}
