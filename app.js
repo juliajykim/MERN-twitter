326eea6fbaa76283bb21304475dcfe0080179cad
juliajykim <jkim24@gwu.edu>
2021-05-24 01:58:25 -0400

    heroku test

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ const tweets = require("./routes/api/tweets");
 //   .then(() => console.log("Connected to MongoDB successfully"))
 //   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!!"));
-
-// app.use(passport.initialize());
-// require('./config/passport')(passport);
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
